73943bdfbd341099029c63220b3575830cb05f3f
John Santos <john_adriel_santos@dlsu.edu.ph>
2021-09-10 23:33:15 +0800

    Added Upgrade Menu in Content (WIP)

--- Assets/Scripts/Upgrades/UpgradeMenu.cs
+++ Assets/Scripts/Upgrades/UpgradeMenu.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class UpgradeMenu : MonoBehaviour
 {
-    private List<UpgradeMenuItem> items;
+    [SerializeField] private List<UpgradeMenuItem> items;
 
-    private void Start()
-    {
-        items = new List<UpgradeMenuItem>(GetComponentsInChildren<UpgradeMenuItem>());
-    }
     public void UpdateAllItems()
     {
         foreach(UpgradeMenuItem item in items)
