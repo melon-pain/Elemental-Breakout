c8f031a77a3e693079f3f4a40eecc951f427d1b3
John Santos <john_adriel_santos@dlsu.edu.ph>
2021-09-16 05:19:57 +0800

    Added User Profile for Creating and Editing.

---
+++ Assets/Scripts/Web/UserAdd.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class UserAdd : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField[] requiredInputs;
+    [SerializeField] private UserProfile userProfileScript;
+    [SerializeField] private Button confirmButton;
+    private bool confirmActive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        confirmActive = true;
+        for (int i = 0; i < requiredInputs.Length; i++)
+        {
+            if(string.IsNullOrEmpty(requiredInputs[i].text))
+            {
+                confirmActive = false;
+            }
+        }
+        if(!confirmActive && confirmButton.interactable)
+        {
+            confirmButton.interactable = false;
+        }
+        else if(confirmActive && !confirmButton.interactable)
+        {
+            confirmButton.interactable = true;
+        }
+    }
+
+    public void AddUser()
+    {
+        Dictionary<string, string> PlayerParams = new Dictionary<string, string>();
+
+        PlayerParams.Add("nickname", requiredInputs[0].text);
+        PlayerParams.Add("name", requiredInputs[1].text);
+        PlayerParams.Add("email", requiredInputs[2].text);
+        PlayerParams.Add("contact", requiredInputs[3].text);
+
+        userProfileScript.CreatePlayer(PlayerParams);
+    }
+
+}
---
+++ Assets/Scripts/Web/UserEdit.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class UserEdit : MonoBehaviour
+{
+
+    [SerializeField] private TMP_InputField[] requiredInputs;
+    [SerializeField] private UserProfile userProfileScript;
+    [SerializeField] private TMP_Dropdown dropDown;
+    [SerializeField] private Button confirmButton;
+    private bool confirmActive;
+    private bool dropDownSelected = false;
+
+    private void Start()
+    {
+        confirmButton.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Dropdown value: " + dropDown.value);
+        confirmActive = true;
+        for (int i = 0; i < requiredInputs.Length; i++)
+        {
+            if (string.IsNullOrEmpty(requiredInputs[i].text))
+            {
+                confirmActive = false;
+            }
+        }
+        if(!dropDownSelected)
+        {
+            confirmActive = false;
+        }
+
+        if (!confirmActive && confirmButton.interactable)
+        {
+            confirmButton.interactable = false;
+        }
+        else if (confirmActive && !confirmButton.interactable)
+        {
+            confirmButton.interactable = true;
+        }
+    }
+
+    public void EditUser()
+    {
+        Dictionary<string, string> PlayerParams = new Dictionary<string, string>();
+
+        PlayerParams.Add("nickname", requiredInputs[0].text);
+        PlayerParams.Add("name", requiredInputs[1].text);
+        PlayerParams.Add("email", requiredInputs[2].text);
+        PlayerParams.Add("contact", requiredInputs[3].text);
+
+        userProfileScript.EditPlayer(PlayerParams, userProfileScript.users[dropDown.value].id);
+        userProfileScript.GetAllPlayers();
+    }
+
+    public void UpdateDropDown()
+    {
+        dropDown.ClearOptions();
+        for (int i = 0; i < userProfileScript.users.Count; i++)
+        {
+            TMP_Dropdown.OptionData newData = new TMP_Dropdown.OptionData();
+            newData.text = userProfileScript.users[i].name;
+            dropDown.options.Add(newData);
+        }
+        ClearFieldInput();
+        dropDownSelected = false;
+    }
+
+    public void OnDropDownChange()
+    {
+        UpdateFieldInput();
+        dropDownSelected = true;
+    }
+
+    public void UpdateFieldInput()
+    {
+        requiredInputs[0].text = userProfileScript.users[dropDown.value].nickname;
+        requiredInputs[1].text = userProfileScript.users[dropDown.value].name;
+        requiredInputs[2].text = userProfileScript.users[dropDown.value].email;
+        requiredInputs[3].text = userProfileScript.users[dropDown.value].contact;
+    }
+
+    public void ClearFieldInput()
+    {
+        for (int i = 0; i < requiredInputs.Length; i++)
+        {
+            requiredInputs[i].text = "";
+        }
+    }
+
+}
